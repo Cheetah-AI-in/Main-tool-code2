efb0b008219323658785c4860bb2c9e51ca518eb
itsANURAG <134876265+itsanuragkumarjha@users.noreply.github.com>
2024-09-02 09:26:25 -0700

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -1,11 +1,11 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-import asyncio
+import threading
 import tool_app  # Import your main script
 import os
 from werkzeug.utils import secure_filename
 
-app = Flask(__name__)
+app = Flask(_name_)
 CORS(app)  # This will enable CORS for all routes
 
 # Configuration
@@ -23,6 +23,13 @@ uploaded_file_path = None
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+def run_script_thread(uploaded_file_path):
+    global script_result
+    if uploaded_file_path:
+        script_result = tool_app.main(uploaded_file_path)
+    else:
+        script_result = {"error": "No file uploaded"}
+
 @app.route('/upload', methods=['POST'])
 def upload_file():
     global uploaded_file_path
@@ -39,27 +46,22 @@ def upload_file():
     return jsonify({'error': 'File type not allowed'}), 400
 
 @app.route('/run-script', methods=['GET'])
-async def run_script():
-    global uploaded_file_path, script_result
+def run_script():
+    global uploaded_file_path
     if not uploaded_file_path:
         return jsonify({"error": "No file uploaded"}), 400
 
-    try:
-        script_result = await asyncio.create_task(tool_app.main(uploaded_file_path))
-        return jsonify({"message": "Script started"}), 202
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+    thread = threading.Thread(target=run_script_thread, args=(uploaded_file_path,))
+    thread.start()
+    return jsonify({"message": "Script started"}), 202
 
 @app.route('/get-result', methods=['GET'])
-async def get_result():
+def get_result():
     global script_result
     if script_result is None:
         return jsonify({"message": "Script still running or hasn't been started"}), 202
-    elif "error" in script_result:
-        return jsonify(script_result), 400
-    else:
-        return jsonify(script_result), 200
+    return jsonify(script_result), 200
 
-if __name__ == '__main__':
+if _name_ == '_main_':
     port = int(os.environ.get('PORT', 10000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)
